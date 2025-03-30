9aec230d73ea732a49590117367b1ff2f01a5f80
xiaowu <2728795232@qq.com>
2025-03-30 15:32:07 +0800

    fix: 修改主内容区域的溢出属性为自动，改善用户界面滚动体验

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -21,7 +21,7 @@ const StyleTopNavLayout = styled.div`
 const Main = styled.main`
     background-color: var(--color-grey-50);
     padding: 4rem 4.8rem 6.4rem;
-    overflow: scroll;
+    overflow: auto;
 `
 
 const Container = styled.div`
