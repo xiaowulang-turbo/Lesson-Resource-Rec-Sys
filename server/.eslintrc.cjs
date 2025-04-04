0e30439ab6711395efdfd01a864889a3bc606195
xiaowu <2728795232@qq.com>
2025-04-04 12:59:28 +0800

    feat: 更新ESLint配置，调整扩展方式并禁用大小写敏感规则，优化代码质量检查

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -3,12 +3,14 @@ module.exports = {
         node: true,
         es2021: true,
     },
-    extends: ['eslint:recommended'],
+    extends: 'eslint:recommended',
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
     rules: {
+        'import/no-case-sensitive': 'off',
+        'import/case-sensitivity': 'off',
         'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
     },
 }
