619b16ba8b0ce54c1f556ae9b6a02efbe46c97f9
xiaowu <2728795232@qq.com>
2025-04-04 13:57:33 +0800

    feat: 在课程路由中新增创建、更新和删除课程的API端点，并添加相应的注释说明

--- server/src/routes/courseRoutes.js
+++ server/src/routes/courseRoutes.js
@@ -3,14 +3,23 @@ import {
     getAllCourses,
     getRecommendedCourses,
     getCourse,
+    createCourse,
+    updateCourse,
+    deleteCourse,
 } from '../controllers/courseController.js'
 
 const router = express.Router()
 
-router.route('/').get(getAllCourses)
+// 获取所有课程
+router.get('/', getAllCourses)
 
-router.route('/recommended').get(getRecommendedCourses)
+// 获取推荐课程
+router.get('/recommended', getRecommendedCourses)
 
-router.route('/:id').get(getCourse)
+// 获取、更新、删除单个课程
+router.route('/:id').get(getCourse).patch(updateCourse).delete(deleteCourse)
+
+// 创建新课程
+router.post('/', createCourse)
 
 export default router
