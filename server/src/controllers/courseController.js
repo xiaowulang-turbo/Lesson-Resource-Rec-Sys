3f31091dd5662e95122df2fb0b5c17189d71a05f
xiaowu <2728795232@qq.com>
2025-04-04 13:57:18 +0800

    feat: 重构课程控制器，使用catchAsync处理异步操作，新增创建、更新和删除课程的功能，优化错误处理

--- server/src/controllers/courseController.js
+++ server/src/controllers/courseController.js
@@ -1,70 +1,94 @@
 import Course from '../models/courseModel.js'
+import catchAsync from '../utils/catchAsync.js'
+import AppError from '../utils/appError.js'
 
 // 获取所有课程
-export const getAllCourses = async (req, res) => {
-    try {
-        const courses = await Course.find()
-        res.status(200).json({
-            status: 'success',
-            results: courses.length,
-            data: {
-                courses,
-            },
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message,
-        })
-    }
-}
+export const getAllCourses = catchAsync(async (req, res, next) => {
+    const courses = await Course.find()
+
+    res.status(200).json({
+        status: 'success',
+        results: courses.length,
+        data: {
+            courses,
+        },
+    })
+})
 
 // 获取推荐课程
-export const getRecommendedCourses = async (req, res) => {
-    try {
-        // 基于评分和注册人数的简单推荐算法
-        const recommendedCourses = await Course.find()
-            .sort({
-                course_rating: -1,
-                course_students_enrolled: -1,
-            })
-            .limit(10)
+export const getRecommendedCourses = catchAsync(async (req, res, next) => {
+    const courses = await Course.find({ isRecommended: true })
 
-        res.status(200).json({
-            status: 'success',
-            results: recommendedCourses.length,
-            data: {
-                courses: recommendedCourses,
-            },
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message,
-        })
+    res.status(200).json({
+        status: 'success',
+        results: courses.length,
+        data: {
+            courses,
+        },
+    })
+})
+
+// 获取单个课程
+export const getCourse = catchAsync(async (req, res, next) => {
+    const course = await Course.findOne({ course_id: req.params.id })
+
+    if (!course) {
+        return next(new AppError('未找到该课程', 404))
     }
-}
 
-// 根据ID获取单个课程
-export const getCourse = async (req, res) => {
-    try {
-        const course = await Course.findOne({ course_id: req.params.id })
-        if (!course) {
-            return res.status(404).json({
-                status: 'fail',
-                message: '未找到该课程',
-            })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            course,
+        },
+    })
+})
+
+// 创建课程
+export const createCourse = catchAsync(async (req, res, next) => {
+    const newCourse = await Course.create(req.body)
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            course: newCourse,
+        },
+    })
+})
+
+// 更新课程
+export const updateCourse = catchAsync(async (req, res, next) => {
+    const course = await Course.findOneAndUpdate(
+        { course_id: req.params.id },
+        req.body,
+        {
+            new: true,
+            runValidators: true,
         }
-        res.status(200).json({
-            status: 'success',
-            data: {
-                course,
-            },
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message,
-        })
+    )
+
+    if (!course) {
+        return next(new AppError('未找到该课程', 404))
     }
-}
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            course,
+        },
+    })
+})
+
+// 删除课程
+export const deleteCourse = catchAsync(async (req, res, next) => {
+    const course = await Course.findOneAndDelete({ course_id: req.params.id })
+
+    if (!course) {
+        return next(new AppError('未找到该课程', 404))
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    })
+})
