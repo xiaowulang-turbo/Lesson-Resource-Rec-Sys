055c98ae28db9bf2937a6ca82917071ace3a21e4
xiaowu <2728795232@qq.com>
2025-04-04 15:15:11 +0800

    feat: 删除预订页面组件以进一步简化项目结构，提升代码可维护性

--- src/pages/Booking.jsx
+++
@@ -1,6 +0,0 @@
-import BookingDetail from "../features/bookings/BookingDetail";
-
-// Do not write side effects in the pages components, only in the feature components
-export default function Booking() {
-  return <BookingDetail />;
-}
