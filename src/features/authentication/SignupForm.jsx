6900a5b1918dc223c7c63c7b9f335bf457d92832
xiaowu <2728795232@qq.com>
2025-04-06 18:40:31 +0800

    feat: 更新SignupForm和UpdatePasswordForm组件，调整代码格式并翻译标签为中文，以提升用户体验

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,95 +1,95 @@
-import { useForm } from "react-hook-form";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
-import useSignup from "./useSignup";
+import { useForm } from 'react-hook-form'
+import Button from '../../ui/Button'
+import Form from '../../ui/Form'
+import FormRow from '../../ui/FormRow'
+import Input from '../../ui/Input'
+import useSignup from './useSignup'
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit, reset } = useForm();
-  const { errors } = formState;
+    const { register, formState, getValues, handleSubmit, reset } = useForm()
+    const { errors } = formState
 
-  const { signup, isLoading } = useSignup();
+    const { signup, isLoading } = useSignup()
 
-  const onSubmit = (data) => {
-    signup(data, {
-      // this should receive a callback function
-      onSettled: () => {
-        reset();
-      },
-    });
-    console.log(data);
-  };
-
-  return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input
-          type="text"
-          id="fullName"
-          {...register("fullName", {
-            required: "FullName is required",
-          })}
-        />
-      </FormRow>
-
-      <FormRow label="Email address" error={errors?.email?.message}>
-        <Input
-          type="email"
-          id="email"
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "Email is not valid",
+    const onSubmit = (data) => {
+        signup(data, {
+            // this should receive a callback function
+            onSettled: () => {
+                reset()
             },
-          })}
-        />
-      </FormRow>
+        })
+        console.log(data)
+    }
 
-      <FormRow
-        label="Password (min 8 characters)"
-        error={errors?.password?.message}
-      >
-        <Input
-          type="password"
-          id="password"
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 8,
-              message: "Password must be at least 8 characters",
-            },
-          })}
-        />
-      </FormRow>
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <FormRow label="Full name" error={errors?.fullName?.message}>
+                <Input
+                    type="text"
+                    id="fullName"
+                    {...register('fullName', {
+                        required: 'FullName is required',
+                    })}
+                />
+            </FormRow>
 
-      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input
-          type="password"
-          id="passwordConfirm"
-          {...register("passwordConfirm", {
-            required: "Password confirmation is required",
-            validate: (value) => {
-              if (value !== getValues("password")) {
-                return "Passwords do not match";
-              }
-            },
-          })}
-        />
-      </FormRow>
+            <FormRow label="Email address" error={errors?.email?.message}>
+                <Input
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                        required: 'Email is required',
+                        pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: 'Email is not valid',
+                        },
+                    })}
+                />
+            </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset" disabled={isLoading}>
-          Cancel
-        </Button>
-        <Button disabled={isLoading}>Create new user</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow
+                label="密码 (至少8个字符)"
+                error={errors?.password?.message}
+            >
+                <Input
+                    type="password"
+                    id="password"
+                    {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 8,
+                            message: 'Password must be at least 8 characters',
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow label="重复密码" error={errors?.passwordConfirm?.message}>
+                <Input
+                    type="password"
+                    id="passwordConfirm"
+                    {...register('passwordConfirm', {
+                        required: 'Password confirmation is required',
+                        validate: (value) => {
+                            if (value !== getValues('password')) {
+                                return 'Passwords do not match'
+                            }
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset" disabled={isLoading}>
+                    Cancel
+                </Button>
+                <Button disabled={isLoading}>Create new user</Button>
+            </FormRow>
+        </Form>
+    )
 }
 
-export default SignupForm;
+export default SignupForm
--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -1,67 +1,64 @@
-import { useForm } from "react-hook-form";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import { useForm } from 'react-hook-form'
+import Button from '../../ui/Button'
+import Form from '../../ui/Form'
+import FormRow from '../../ui/FormRow'
+import Input from '../../ui/Input'
 
-import useUpdateUser from "./useUpdateUser";
+import useUpdateUser from './useUpdateUser'
 
 function UpdatePasswordForm() {
-  const { register, handleSubmit, formState, getValues, reset } = useForm();
-  const { errors } = formState;
+    const { register, handleSubmit, formState, getValues, reset } = useForm()
+    const { errors } = formState
 
-  const { updateUser, isUpdating } = useUpdateUser();
+    const { updateUser, isUpdating } = useUpdateUser()
 
-  function onSubmit({ password }) {
-    updateUser({ password }, { onSuccess: reset });
-  }
+    function onSubmit({ password }) {
+        updateUser({ password }, { onSuccess: reset })
+    }
 
-  return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow
-        label="Password (min 8 characters)"
-        error={errors?.password?.message}
-      >
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          disabled={isUpdating}
-          {...register("password", {
-            required: "This field is required",
-            minLength: {
-              value: 8,
-              message: "New password needs a minimum of 8 characters",
-            },
-          })}
-        />
-      </FormRow>
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <FormRow
+                label="密码 (至少8个字符)"
+                error={errors?.password?.message}
+            >
+                <Input
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    disabled={isUpdating}
+                    {...register('password', {
+                        required: 'This field is required',
+                        minLength: {
+                            value: 8,
+                            message: '密码需要至少8个字符',
+                        },
+                    })}
+                />
+            </FormRow>
 
-      <FormRow
-        label="Confirm password"
-        error={errors?.passwordConfirm?.message}
-      >
-        <Input
-          type="password"
-          autoComplete="new-password"
-          id="passwordConfirm"
-          disabled={isUpdating}
-          {...register("passwordConfirm", {
-            required: "This field is required",
-            validate: (value) =>
-              getValues().password === value || "Passwords need to match",
-          })}
-        />
-      </FormRow>
+            <FormRow label="确认密码" error={errors?.passwordConfirm?.message}>
+                <Input
+                    type="password"
+                    autoComplete="new-password"
+                    id="passwordConfirm"
+                    disabled={isUpdating}
+                    {...register('passwordConfirm', {
+                        required: 'This field is required',
+                        validate: (value) =>
+                            getValues().password === value || '密码需要匹配',
+                    })}
+                />
+            </FormRow>
 
-      <FormRow>
-        <Button onClick={reset} type="reset" variation="secondary">
-          Cancel
-        </Button>
-        <Button disabled={isUpdating}>Update password</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow>
+                <Button onClick={reset} type="reset" variation="secondary">
+                    取消
+                </Button>
+                <Button disabled={isUpdating}>更新密码</Button>
+            </FormRow>
+        </Form>
+    )
 }
 
-export default UpdatePasswordForm;
+export default UpdatePasswordForm
