5b35a7c7af80063570b60ebbd01028659171eecc
xiaowu <2728795232@qq.com>
2025-04-06 18:40:24 +0800

    fix: 修改main.jsx文件，注释掉React.StrictMode以简化渲染过程

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from './ui/ErrorFallback.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
     <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => window.location.replace("/")}
+        FallbackComponent={ErrorFallback}
+        onReset={() => window.location.replace('/')}
     >
-      <App />
+        <App />
     </ErrorBoundary>
-  </React.StrictMode>
-);
+    // </React.StrictMode>
+)
