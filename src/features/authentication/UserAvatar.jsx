21820edf6df26c4acc3aba9bc1d9d03007056f2b
xiaowu <2728795232@qq.com>
2025-04-13 18:21:00 +0800

    feat: 更新UserAvatar组件，添加点击头像跳转到账户页面的功能，并优化代码格式

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,37 +1,44 @@
-import styled from "styled-components";
-import useUser from "./useUser";
+import styled from 'styled-components'
+import useUser from './useUser'
+import { useNavigate } from 'react-router-dom'
 
 const StyledUserAvatar = styled.div`
-  display: flex;
-  gap: 1.2rem;
-  align-items: center;
-  font-weight: 500;
-  font-size: 1.4rem;
-  color: var(--color-grey-600);
-`;
+    display: flex;
+    gap: 1.2rem;
+    align-items: center;
+    font-weight: 500;
+    font-size: 1.4rem;
+    color: var(--color-grey-600);
+    cursor: pointer;
+`
 
 const Avatar = styled.img`
-  display: block;
-  width: 4rem;
-  width: 3.6rem;
-  aspect-ratio: 1;
-  object-fit: cover;
-  object-position: center;
-  border-radius: 50%;
-  outline: 2px solid var(--color-grey-100);
-`;
+    display: block;
+    width: 4rem;
+    width: 3.6rem;
+    aspect-ratio: 1;
+    object-fit: cover;
+    object-position: center;
+    border-radius: 50%;
+    outline: 2px solid var(--color-grey-100);
+`
 
 export default function UserAvatar() {
-  const { user } = useUser();
-  const { fullName, avatar } = user?.user_metadata || {};
+    const navigate = useNavigate()
+    const { user } = useUser()
+    const { fullName, avatar } = user?.user_metadata || {}
 
-  return (
-    <StyledUserAvatar>
-      <Avatar
-        src={avatar || "default-user.jpg"}
-        alt={`Avatar of ${fullName}`}
-      />
-      <span>{fullName}</span>
-    </StyledUserAvatar>
-  );
+    const handleAvatarClick = () => {
+        navigate('/account')
+    }
+
+    return (
+        <StyledUserAvatar onClick={handleAvatarClick}>
+            <Avatar
+                src={avatar || 'default-user.jpg'}
+                alt={`Avatar of ${fullName}`}
+            />
+            <span>{fullName}</span>
+        </StyledUserAvatar>
+    )
 }
