ff46883bf1067c60794053cef66e6ea0a1785044
xiaowu <2728795232@qq.com>
2025-04-13 18:21:05 +0800

    feat: 优化HeaderMenu组件的代码格式，注释掉不必要的代码以提升可读性

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,31 +1,31 @@
-import { HiOutlineUser } from "react-icons/hi2";
-import ButtonIcon from "./ButtonIcon";
-import Logout from "./Logout";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import DarkModeToggle from "./DarkModeToggle";
+import { HiOutlineUser } from 'react-icons/hi2'
+import ButtonIcon from './ButtonIcon'
+import Logout from './Logout'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import DarkModeToggle from './DarkModeToggle'
 
 const StyledHeaderMenu = styled.ul`
-  display: flex;
-  gap: 0.4rem;
-`;
+    display: flex;
+    gap: 0.4rem;
+`
 
 export default function HeaderMenu() {
-  const navigate = useNavigate();
+    const navigate = useNavigate()
 
-  return (
-    <StyledHeaderMenu>
-      <li>
+    return (
+        <StyledHeaderMenu>
+            {/* <li>
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
-      </li>
-      <li>
-        <DarkModeToggle />
-      </li>
-      <li>
-        <Logout />
-      </li>
-    </StyledHeaderMenu>
-  );
+      </li> */}
+            <li>
+                <DarkModeToggle />
+            </li>
+            <li>
+                <Logout />
+            </li>
+        </StyledHeaderMenu>
+    )
 }
