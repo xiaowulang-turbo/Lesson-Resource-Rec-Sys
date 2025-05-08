d6e78d1f66f39cb8e139996c9798f3cd02bf1069
ruanxiaowu <2728795232@qq.com>
2025-05-08 21:43:35 +0800

    feat: 在Empty组件中添加中文提示信息，增强用户体验

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 function Empty({ resource }) {
     return <p>No {resource} could be found.</p>
+    // return <p>暂无{resource}数据</p>
 }
 
 export default Empty
