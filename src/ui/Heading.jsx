4b82ee7a18a6c616eb960d087f3417a44f18a081
xiaowu <2728795232@qq.com>
2025-05-12 15:17:45 +0800

    refactor: 统一Heading和Row组件中的样式引号格式，提升代码一致性

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,38 +1,38 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
 const Heading = styled.h1`
-  ${(props) =>
-    props.type === "h1" &&
-    css`
-      font-size: 3rem;
-      font-weight: 600;
-    `}
+    ${(props) =>
+        props.type === 'h1' &&
+        css`
+            font-size: 3rem;
+            font-weight: 600;
+        `}
+
+    ${(props) =>
+        props.type === 'h2' &&
+        css`
+            font-size: 2rem;
+            font-weight: 600;
+        `}
 
   ${(props) =>
-    props.type === "h2" &&
-    css`
-      font-size: 2rem;
-      font-weight: 600;
-    `}
+        props.type === 'h3' &&
+        css`
+            font-size: 2rem;
+            font-weight: 500;
+        `}
 
   ${(props) =>
-    props.type === "h3" &&
-    css`
-      font-size: 2rem;
-      font-weight: 500;
-    `}
-
-  ${(props) =>
-    props.type === "h4" &&
-    css`
-      font-size: 3rem;
-      font-weight: 600;
-      text-align: center;
-    `}
+        props.type === 'h4' &&
+        css`
+            font-size: 3rem;
+            font-weight: 600;
+            text-align: center;
+        `}
 
   font-size: 20px;
-  font-weight: 600;
-  /* background-color: yellow; */
-`;
+    font-weight: 600;
+    /* background-color: yellow; */
+`
 
-export default Heading;
+export default Heading
--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,25 +1,25 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
 const Row = styled.div`
-  display: flex;
+    display: flex;
 
-  ${(props) =>
-    props.type === "horizontal" &&
-    css`
-      justify-content: space-between;
-      align-items: center;
-    `}
+    ${(props) =>
+        props.type === 'horizontal' &&
+        css`
+            justify-content: space-between;
+            align-items: center;
+        `}
 
-  ${(props) =>
-    props.type === "vertical" &&
-    css`
-      flex-direction: column;
-      gap: 1.6rem;
-    `}
-`;
+    ${(props) =>
+        props.type === 'vertical' &&
+        css`
+            flex-direction: column;
+            gap: 1.6rem;
+        `}
+`
 
 Row.defaultProps = {
-  type: "vertical",
-};
+    type: 'vertical',
+}
 
-export default Row;
+export default Row
