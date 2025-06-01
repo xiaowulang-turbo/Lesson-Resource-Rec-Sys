1bbb2236877e9e7b3ae7f3538d385618aca00456
ruanxiaowu <2728795232@qq.com>
2025-06-01 16:11:45 +0800

    feat: 更新确认删除组件，优化样式并翻译文本为中文，提升用户体验

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -1,57 +1,58 @@
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-import Button from "./Button";
-import Heading from "./Heading";
+import Button from './Button'
+import Heading from './Heading'
 
 const StyledConfirmDelete = styled.div`
-  width: 40rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1.2rem;
-
-  & p {
-    color: var(--color-grey-500);
-    margin-bottom: 1.2rem;
-  }
-
-  & div {
+    width: 40rem;
     display: flex;
-    justify-content: flex-end;
+    flex-direction: column;
     gap: 1.2rem;
-  }
-`;
+
+    & p {
+        color: var(--color-grey-500);
+        margin-bottom: 1.2rem;
+    }
+
+    & div {
+        display: flex;
+        justify-content: flex-end;
+        gap: 1.2rem;
+    }
+`
 
 ConfirmDelete.propTypes = {
-  resourceName: PropTypes.string,
-  onConfirm: PropTypes.func,
-  disabled: PropTypes.bool,
-  onCloseModal: PropTypes.func,
-};
-
-function ConfirmDelete({ resourceName, onConfirm, disabled, onCloseModal }) {
-  return (
-    <StyledConfirmDelete>
-      <Heading as="h3">Delete {resourceName}</Heading>
-      <p>
-        Are you sure you want to delete this {resourceName} permanently? This
-        action cannot be undone.
-      </p>
-
-      <div>
-        <Button
-          variation="secondary"
-          disabled={disabled}
-          onClick={onCloseModal}
-        >
-          Cancel
-        </Button>
-        <Button variation="danger" disabled={disabled} onClick={onConfirm}>
-          Delete
-        </Button>
-      </div>
-    </StyledConfirmDelete>
-  );
+    resourceName: PropTypes.string,
+    onConfirm: PropTypes.func,
+    disabled: PropTypes.bool,
+    onCloseModal: PropTypes.func,
 }
 
-export default ConfirmDelete;
+function ConfirmDelete({ resourceName, onConfirm, disabled, onCloseModal }) {
+    return (
+        <StyledConfirmDelete>
+            <Heading as="h3">删除{resourceName}</Heading>
+            <p>确定要删除该{resourceName}吗？此操作无法撤销。</p>
+
+            <div>
+                <Button
+                    variation="secondary"
+                    disabled={disabled}
+                    onClick={onCloseModal}
+                >
+                    取消
+                </Button>
+                <Button
+                    variation="danger"
+                    disabled={disabled}
+                    onClick={onConfirm}
+                >
+                    确认删除
+                </Button>
+            </div>
+        </StyledConfirmDelete>
+    )
+}
+
+export default ConfirmDelete
