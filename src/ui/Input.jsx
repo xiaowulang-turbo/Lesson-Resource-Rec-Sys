e19b32870539c23427ec15f828d93e9e8fd4fffb
ruanxiaowu <2728795232@qq.com>
2025-06-01 15:24:28 +0800

    feat: 更新输入组件背景色，提升视觉效果

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 const Input = styled.input`
     border: 1px solid var(--color-grey-300);
-    background-color: var(--color-grey-100);
+    background-color: var(--color-grey-0);
     border-radius: 4px;
     padding: 10px;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
